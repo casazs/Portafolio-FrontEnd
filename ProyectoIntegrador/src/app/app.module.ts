89053c9cc4bf78fc6bd07ca522c63136e410eadc
Mateo casas <mateoc.ds0@gmail.com>
2022-11-21 16:37:28 -0300

    commit

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArPrComponent } from './components/logo-ar-pr/logo-ar-pr.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArPrComponent,
+    SocialsComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
